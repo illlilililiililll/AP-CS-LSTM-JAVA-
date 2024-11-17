5826a71bf36ba282b22f4e71eba5854d327cabc3
illlilililiililll <kcwrenew22c@gmail.com>
2024-11-17 17:37:07 +0900

    20241117 add comment

--- src/Adam.java
+++ src/Adam.java
@@ -1,13 +1,17 @@
 public class Adam {
+    // Weight, Bias 행렬 m, v 초기화
     private double[][] mWf, mWi, mWC, mWo, mbf, mbi, mbC, mbo;
     private double[][] vWf, vWi, vWC, vWo, vbf, vbi, vbC, vbo;
-
+    
+    // 학습속도조절 - beta1, beta2, learning rate 초기화 및 설정
+    // 오류 방지 - epsilon 정의
     private final double beta1 = 0.9;
     private final double beta2 = 0.999;
     private final double epsilon = 1e-8;
     private final double learningRate = 0.001;
     private int t = 0;
 
+    // m, v행렬 초기값 설정
     public Adam(int hidden, int input) {
         // Initialize moments for weights and biases
         mWf = NumJava.zeros(hidden, hidden + input);
@@ -31,6 +35,7 @@ public class Adam {
         vbo = NumJava.zeros(hidden, 1);
     }
 
+    // 단계 진행
     private double[][] update(double[][] param, double[][] grad, double[][] m, double[][] v) {
         t += 1;
 
@@ -43,7 +48,7 @@ public class Adam {
         return NumJava.subtract(param, NumJava.times(learningRate, NumJava.div(mHat, NumJava.add(NumJava.sqrt(vHat), epsilon)))); // 새로운 값을 반환
     }
 
-
+    // LSTM 모델 가중치 적용 (업데이트)
     public void apply(LSTM lstm, double[][] dWf, double[][] dWi, double[][] dWC, double[][] dWo,
                                  double[][] dbf, double[][] dbi, double[][] dbC, double[][] dbo) {
 
--- src/LSTM.java
+++ src/LSTM.java
@@ -19,22 +19,27 @@ public class LSTM {
         this.adam = new Adam(hidden, input);
     }
     
-    // 순방향 전파 
+    // 순방향 전파
     double[][][] forward(double[][] x, double[][] h_prev, double[][] C_prev) {
         double[][] concat = NumJava.vstack(h_prev, x);
 
+        // Forget Gate
         double[][] f_t = NumJava.sigmoid(NumJava.add(NumJava.dot(Wf, concat), bf));
 
+        // Input Gate
         double[][] i_t = NumJava.sigmoid(NumJava.add(NumJava.dot(Wi, concat), bi));
+
+        // Cell State Candidate
         double[][] C_tilda = NumJava.tanh(NumJava.add(NumJava.dot(WC, concat), bC));
 
+        // Cell State
         double[][] C_t = NumJava.add(NumJava.times(f_t, C_prev), NumJava.times(i_t, C_tilda));
 
+        // Output Gate
         double[][] o_t = NumJava.sigmoid(NumJava.add(NumJava.dot(Wo, concat), bo));
 
+        // Hidden State
         double[][] h_t = NumJava.times(o_t, NumJava.tanh(C_t));
-
-        h_t = NumJava.softmax(h_t);
 
         return new double[][][] { f_t, i_t, C_tilda, o_t, C_t, h_t };
     }
@@ -52,6 +57,7 @@ public class LSTM {
             double[][] h_prev,
             double[][] x
     ) {
+        // 오차역전파를 통한 편미분 계산
         // Output Gate
         double[][] do_t = NumJava.times(NumJava.times(dh_next, NumJava.tanh(C_t)), NumJava.dsigmoid(o_t));
 
@@ -69,6 +75,7 @@ public class LSTM {
 
         double[][] concat = NumJava.vstack(h_prev, x);
 
+        // 가중치 미분 정의
         double[][] T = NumJava.transpose(concat);
         double[][] dWf = NumJava.dot(df_t, T);
         double[][] dWi = NumJava.dot(di_t, T);
@@ -93,14 +100,18 @@ public class LSTM {
     
     // 모델 학습
     public void fit(double[][][] X, double[][][] Y, int epochs) {
-        for (int epoch = 0; epoch < epochs; epoch++) {
-            double loss = 0.0;
+        for (int epoch = 0; epoch < epochs; epoch++) { // epoch (반복횟수) 만큼 반복
+            double loss = 0.0; // 오차
+
+            // 이전 Hidden State, Cell State 초기화
             double[][] h_prev = NumJava.zeros(Wf.length, 1);
             double[][] C_prev = NumJava.zeros(WC.length, 1);
 
-            for (int t = 0; t < X.length; t++) {
+            for (int t = 0; t < X.length; t++) { // Time Step (X 행렬 행) 만큼 반복
                 double[][] x = X[t];
                 double[][] y_real = Y[t];
+
+                // 순전파
                 double[][][] fwd = forward(x, h_prev, C_prev);
 
                 double[][] f_t = fwd[0];
@@ -110,13 +121,15 @@ public class LSTM {
                 double[][] C_t = fwd[4];
                 double[][] h_t = fwd[5];
 
-                double[][] pred = NumJava.softmax(h_t);
+                double[][] pred = NumJava.softmax(h_t); // SoftMax 적용
 
-                loss += NumJava.crossEntropyLoss(pred, y_real);
+                loss += NumJava.crossEntropyLoss(pred, y_real); // CrossEntropyLoss 오차
 
+                // 다음 Hidden State, Cell State 정의
                 double[][] dh_next = NumJava.subtract(pred, y_real);
                 double[][] dC_next = NumJava.zeros(C_t.length, C_t[0].length);
 
+                // 역전파
                 double[][][] backProp = backward(dh_next, dC_next, C_prev, f_t, i_t, C_tilda, o_t, C_t, h_prev, x);
 
                 double[][] dh_prev = backProp[0];
@@ -130,6 +143,7 @@ public class LSTM {
                 double[][] dbC = backProp[8];
                 double[][] dbo = backProp[9];
 
+                // 가중치 업데이트
                 adam.apply(this, dWf, dWi, dWC, dWo, dbf, dbi, dbC, dbo);
 
                 h_prev = h_t;
